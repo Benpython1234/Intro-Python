6c87b66a76f837c996d1c3e57fe43025db33b43f
Benpython1234 <benjoash15@gmail.com>
2024-12-16 18:28:40 +1100

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,14 @@
+#algorithm
+# add to mumbers 
+
+print("step1: define variable and assign value x,y")
+print("step2: add two numbers and assign it to sum vriable,sum = x+y")
+print("step3: display sum")
+
+name='Ben'
+school_name='Glen Waverly Secoundry college'
+age=13
+sport='soccer'
+
+print(name)
+print(f'My name is {name}, I go to {school_name}, my age is {age}, the sport I play is {sport}')
